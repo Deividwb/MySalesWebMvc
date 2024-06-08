965643b02d9ae7d65cdcf67bf679abc6d47bb792
Deividwb <bernardinodeivid@gmail.com>
2024-06-07 23:10:04 -0300

    crud scaffolding

---
+++ MySalesWebMvc/Data/MySalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySalesWebMvc.Models;
+
+namespace MySalesWebMvc.Data
+{
+    public class MySalesWebMvcContext : DbContext
+    {
+        public MySalesWebMvcContext (DbContextOptions<MySalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MySalesWebMvc.Models.Department> Department { get; set; } = default!;
+    }
+}
--- MySalesWebMvc/Program.cs
+++ MySalesWebMvc/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySalesWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MySalesWebMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MySalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'MySalesWebMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
